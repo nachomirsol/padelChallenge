5d1c5c217aae337213f1e4491b021e1f3ca05de3
Nacho <nachomirsol@gmail.com>
2022-02-16 15:53:14 +0100

    Update usePhotos.ts

--- src/hooks/usePhotos.ts
+++ src/hooks/usePhotos.ts
@@ -1,7 +1,6 @@
 import { useState, useEffect } from 'react';
 /** Libraries */
-import { useSelector } from 'react-redux';
-import { useDispatch } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 /** Actions */
 import {
 	filterPhotos,
